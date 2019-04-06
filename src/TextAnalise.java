fc882fab3b9dca1bb81400bc423a1a68ca074983
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2019-04-06 15:26:37 +0300

    Проект сделан в idea.

---
+++ src/TextAnalise.java
@@ -0,0 +1,9 @@
+// Графическое приложение
+public class TextAnalise {
+
+    // Главный метод
+    public static void main(String[] args) {
+        MainForm app = new MainForm();
+        app.run();
+    }
+}
